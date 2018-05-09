d415a80691be98d27f26982290dc1b649ce3b817
jbristol85 <josh.bristol2@gmail.com>
2018-05-08 22:43:39 -0400

    finished Mod1_Lab2

--- Mod1_Lab1/myApp/Program.cs
+++ Mod1_Lab1/myApp/Program.cs
@@ -16,6 +16,13 @@ namespace Mod1_Lab1
 
             //output to the console window
             Console.WriteLine($"This car is painted {Car1.Color}, was built in {Car1.Year}, and has {Car1.Mileage} miles on it.");
+            var Car2 = new Car("Red", 2008);
+
+            //access static members
+            int carCount = Car.CountCars();
+
+            //output to the console
+            Console.WriteLine($"There are {carCount} cars in inventory right now.");
         }
     }
 
@@ -26,5 +33,38 @@ namespace Mod1_Lab1
         public string Color { get; set; }
         public int Year { get; set; }
         public int Mileage { get; set; }
+
+        // adding a constructor
+        //this constructor instantiates a Car() object while only having the color and year info
+        public Car(string color, int year)
+        {
+            this.Color = color;
+            this.Year = year;
+            // every time the constructor runs, increment "instances"
+            instances++;
+        }
+
+        // another constructor that will accept only a year and mileage
+        public Car(int year, int mileage)
+        {
+            this.Year = year;
+            this.Mileage = mileage;
+            // every time the constructor runs, increment "instances"
+            instances++;
+        }
+
+        //creates int variable called "Instances and assigns value to 0
+        private static int instances = 0;
+
+        public Car()
+        {
+            //every time the constructor runs, increment "Instances"
+            instances++;
+        }
+        //declare static member
+        public static int CountCars()
+        {
+            return instances;
+        }
     }
 }
